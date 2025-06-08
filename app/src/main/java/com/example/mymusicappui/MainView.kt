1b1d101908aeef98e31256f93ff6b4913f0018be
laura <laura.carolpavani@gmail.com>
2025-06-09 06:51:34 +1000

    Making the drawer appear

--- app/src/main/java/com/example/mymusicappui/MainView.kt
+++ app/src/main/java/com/example/mymusicappui/MainView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
@@ -13,15 +15,21 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -30,6 +38,16 @@ import kotlinx.coroutines.launch
 fun MainView(){
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+
+    //Allow us to find out on which "View" we current are
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val title = remember{
+        //TODO change that to currentScreen.title
+        mutableStateOf("")
+    }
 
     Scaffold(topBar = {
         TopAppBar(title = { Text( "Home") },
@@ -40,10 +58,29 @@ fun MainView(){
                 }
             }) {
                 Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+            }}
+        )
+    }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)){
+                items(screensInDrawer){
+                    item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account"){
+                            //open dialog
+                        }else{
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
+        }
 
-        }}
-            )
-    }) {
+        ) {
         Text("Text", modifier = Modifier.padding(it))
     }
 }
@@ -58,8 +95,9 @@ fun DrawerItem(
     Row(
         Modifier
             .fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 16.dp).background(background)
-            .clickable{
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable {
                 onDrawerItemClicked()
 
             }){
@@ -72,5 +110,5 @@ fun DrawerItem(
         style = MaterialTheme.typography.h5,
         )
     }
-    )
+
 }
--- app/src/main/java/com/example/mymusicappui/Screen.kt
+++ app/src/main/java/com/example/mymusicappui/Screen.kt
@@ -22,3 +22,8 @@ sealed class Screen(val title: String, val route: String) {
         )
         }
 }
+val screensInDrawer = listOf(
+    Screen.DrawerScreen.Account,
+    Screen.DrawerScreen.Subscription,
+    Screen.DrawerScreen.AddAccount
+)
