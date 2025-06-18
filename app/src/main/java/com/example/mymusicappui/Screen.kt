4b72a7bee11324c7d6f326aa858657b18a58af5e
laura <laura.carolpavani@gmail.com>
2025-06-18 14:36:29 +1000

    Setting up the BottomBar

--- app/src/main/java/com/example/mymusicappui/Screen.kt
+++ app/src/main/java/com/example/mymusicappui/Screen.kt
@@ -3,6 +3,13 @@ package com.example.mymusicappui
 import androidx.annotation.DrawableRes
 
 sealed class Screen(val title: String, val route: String) {
+    sealed class BottomScreen(
+        val bTitle: String, val bRoute: String, @DrawableRes val icon: Int
+    ):Screen(bTitle, bRoute){
+        object Home: BottomScreen("Home", "home", R.drawable.baseline_music_video_24)
+        object Library: BottomScreen("Library", "library", R.drawable.baseline_library_music_24)
+        object Browse: BottomScreen("Browse", "browse", R.drawable.baseline_apps_24)
+    }
     sealed class DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes val icon: Int)
         : Screen(dTitle,dRoute){
             object Account: DrawerScreen(
@@ -22,6 +29,13 @@ sealed class Screen(val title: String, val route: String) {
         )
         }
 }
+
+val screensInBottom = listOf(
+    Screen.BottomScreen.Home,
+    Screen.BottomScreen.Browse,
+    Screen.BottomScreen.Library
+)
+
 val screensInDrawer = listOf(
     Screen.DrawerScreen.Account,
     Screen.DrawerScreen.Subscription,
