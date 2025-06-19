12334cc44791e84905151efb2a725c8839b03c2f
laura <laura.carolpavani@gmail.com>
2025-06-19 22:21:32 +1000

    Fixing the Titles when changing the page to reflect the correct page name on it

--- app/src/main/java/com/example/mymusicappui/MainView.kt
+++ app/src/main/java/com/example/mymusicappui/MainView.kt
@@ -99,7 +99,9 @@ fun MainView(){
                     val tint = if(isSelected)Color.White else Color.Black
 
                     BottomNavigationItem(selected = currentRoute == item.bRoute,
-                        onClick = { controller.navigate(item.bRoute) }, icon = {
+                        onClick = { controller.navigate(item.bRoute)
+                                  title.value = item.bTitle
+                                  }, icon = {
                             Icon(tint = tint,
                                 contentDescription = item.bTitle, painter = painterResource(id = item.icon))
                         },
